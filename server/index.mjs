e851cc7eedfc71eb39a559ca04259a14fde48c30
emelieklund <emelieeeklund@gmail.com>
2024-11-20 22:52:05 +0100

    A simple Node Express server initialized

---
+++ server/index.mjs
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 1337;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
