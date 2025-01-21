638b5e2a41b2de6f08b0dacbbcd913abc54893a2
emelieklund <emelieeeklund@gmail.com>
2025-01-21 15:30:20 +0100

    Updated footer layout

--- client/src/app/components/Footer.tsx
+++ client/src/app/components/Footer.tsx
@@ -15,30 +15,26 @@ export default function Footer() {
                     <h1>SEAB</h1>
                     <p>Vi underlättar din vardag</p>
                 </div>
-
-                    <label htmlFor='username' className="new-letter">Prenumerera på vårt nyhetsbrev:
-                        <input
-                            id="username"
-                            type="text"
-                            name="username"
-                            placeholder="Mailadress"
-                            className="email-input"
-                        />
-                        <button id="subscribe-btn" onClick={() => window.location.reload()} ><FontAwesomeIcon icon={faArrowRightFromBracket} /></button>
-                    </label>
-
+                <label htmlFor='username' className="new-letter">Prenumerera på vårt nyhetsbrev:
+                    <input
+                        id="username"
+                        type="text"
+                        name="username"
+                        placeholder="Mailadress"
+                        className="email-input"
+                    />
+                    <button id="subscribe-btn" onClick={() => window.location.reload()} ><FontAwesomeIcon icon={faArrowRightFromBracket} /></button>
+                </label>
             </div>
             <div className='item2'>
                 © Wizards on Wheels
             </div>
             <div className="item3" >
-                <div className="footer-links" >
-                    <p>Om oss</p>
-                    <p>Prislista</p>
-                    <p>FAQ</p>
-                    <p>Kundsupport</p>
-                    <p>Användarvillkor</p>
-                </div>
+                <p>Om oss</p>
+                <p>Prislista</p>
+                <p>FAQ</p>
+                <p>Kundsupport</p>
+                <p>Användarvillkor</p>
             </div>
         </div>
     )
