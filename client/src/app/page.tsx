b4f5441cd371024f5767fdc07f3c16c30b90f991
emelieklund <emelieeeklund@gmail.com>
2025-01-23 15:15:46 +0100

    Small fix

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,4 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import Image from 'next/image'
-import Link from 'next/link'
+import Link from 'next/link';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
@@ -22,28 +19,3 @@ export default function Home() {
     </div>
   )
 }
-=======
-=======
->>>>>>> Stashed changes
-import Link from 'next/link'
-import Footer from './components/Footer';
-import Header from './components/Header';
-
-export default function Home() {
-  return (
-    // <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
-    <div>  
-      <Header />
-      <main>
-        <ul className='temporary-links'>
-          {/* Temporary links!! */}
-          <Link href="/admin">Admin (tillfällig länk)</Link>
-          <Link href="/user/login">Logga in (ska flyttas)</Link>
-          <Link href="/mobileapp">App (tillfällig länk)</Link>
-        </ul>
-      </main>
-      <Footer />
-    </div>
-  )
-}
->>>>>>> Stashed changes
