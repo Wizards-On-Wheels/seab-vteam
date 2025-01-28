5e1bfa198ddaf1ff72c0deb3408e2f8e5454fe65
Shad-e <ljs@Linus.>
2025-01-28 17:48:52 +0100

    Minor changes to BikeTable.tsx

--- client/src/app/admin/bikes/page.tsx
+++ client/src/app/admin/bikes/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import BikeTable from "../../components/AdminBikeTable";
+import BikeTable from "../../components/BikeTable";
 import Link from "next/link";
 
 export default function AdminCyklar() {
